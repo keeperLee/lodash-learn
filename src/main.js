5ea5988c5897cf27095898af6b2ce5318ff7d3fa
lijian <1376623234@qq.com>
2021-06-23 13:06:16 +0800

    引入lodash,调用拼接数组的方法

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import _ from 'lodash'
 Vue.config.productionTip = false
-
+Vue.prototype._= _
 new Vue({
   router,
   store,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+        {{arr1}}
+      <button @click="concatArray">拼接数组</button>
   </div>
 </template>
+
+<script>
+
+    export default {
+        name: 'About',
+        data(){
+            return{
+                arr1:[1,2,3,4,5,6,7]
+            }
+        },
+        methods:{
+            concatArray(){
+                this.arr1 = _.concat(this.arr1,6,5,4,3,2,1)
+            }
+        },
+        mounted() {
+            // this.concatArray();
+        }
+    }
+</script>
+
+
+<style lang="stylus">
+.about
+    font-family Avenir, Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
+</style>
