69d20d51ee420aa9becdda3ab0415c0f94a48836
lijian <1376623234@qq.com>
2021-07-07 14:52:42 +0800

    vue中的emit使用

---
+++ src/components/Son.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="send">sendEmit</button>
+</template>
+
+<script>
+export default {
+    name: "Son",
+    methods:{
+        send(){
+            this.$emit("sendEmit",100);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
